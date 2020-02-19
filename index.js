2b315b26d87f9a0f70b5d74cb5dcbbbaff7c6166
benjchand <benchand@gmail.com>
2020-02-18 16:34:35 -0800

    Done with 4th test.

--- index.js
+++ index.js
@@ -91,8 +91,8 @@ function processLastItem(stringList, callback) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
-  /* CODE HERE */
+function processSum(numberList, callback) {
+  return callback(numberList.reduce((total, current) => total + current, 0))
 }
 
 /**
